5a97b78294d96f59a3ca362c441b5b569bc02824
Hrishikesh Belgaumkar <hribgm2496@gmail.com>
2019-07-31 13:28:32 +0530

    Updates

---
+++ precache-manifest.e7f77f64b2ff7a2c0c4a7a2afd1fbba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e38d16d69d1c9bd57ddffbc3b3a6df",
+    "url": "/invoice-react/index.html"
+  },
+  {
+    "revision": "28ac8f8346c8109071d0",
+    "url": "/invoice-react/static/css/main.b65b3b76.chunk.css"
+  },
+  {
+    "revision": "5c56a8a53fb51318fc68",
+    "url": "/invoice-react/static/js/2.f865540e.chunk.js"
+  },
+  {
+    "revision": "28ac8f8346c8109071d0",
+    "url": "/invoice-react/static/js/main.5742b0e7.chunk.js"
+  },
+  {
+    "revision": "457e679aafd59929a522",
+    "url": "/invoice-react/static/js/runtime~main.ac1d9015.js"
+  }
+]);
